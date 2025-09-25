3c81ab66840dc247b1eafd4390f52713aaeec34f
NazaNEYn <jigar_hengameh@yahoo.com>
2025-09-25 21:21:36 +0330

    Added the unit-converter file

---
+++ scripts/unit-converter/length_conversions.py
@@ -0,0 +1,18 @@
+#  and in_to_cm()
+
+# miles to kilometers
+# 1 mile = 1.60934 kilometers
+
+
+def miles_to_km(miles):
+    km = 1.60934
+    return miles * km
+
+
+# inches to centimeter
+# 1 inch = 2.54 centimeters
+
+
+def inches_to_centimeter(inch):
+    cm = 2.54
+    return inch * cm
---
+++ scripts/unit-converter/temperature_conversions.py
@@ -0,0 +1,5 @@
+# fahrenheit to celsius
+
+
+def fahrenheit_to_celsius(far):
+    return (far - 32) * 5 / 9
