9a12cf8c1f424525e01ee054e01623fcfc55b8d8
NazaNEYn <jigar_hengameh@yahoo.com>
2025-09-26 13:15:49 +0330

    fix: removed leftover commentes and the unnecessary files and moved them to utils.py

--- scripts/unit-converter/temperature_conversions.py
+++
@@ -1,5 +0,0 @@
-# fahrenheit to celsius
-
-
-def fahrenheit_to_celsius(far):
-    return (far - 32) * 5 / 9
---
+++ scripts/unit-converter/unit_converter/utils.py
@@ -0,0 +1,10 @@
+def miles_to_km(miles):
+    return miles * 1.60934
+
+
+def inches_to_centimeter(inches):
+    return inches * 2.54
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
